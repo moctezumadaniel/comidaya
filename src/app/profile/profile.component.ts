7ba8bacfbc00e7c10691132e5954d3472860170d
moctezumadaniel <moctezumadanielt@gmail.com>
2021-11-09 19:08:58 -0600

    initial profile settings component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
+  styleUrls: ['./profile.component.sass'],
 })
 export class ProfileComponent implements OnInit {
+  userName: string = '';
+  address: string = '';
+  tradename: string = '';
+  businessAddress: string = '';
+  oldPassword: string = '';
+  password: string = '';
+  newPassword: string = '';
+  confirmNewPassword: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
