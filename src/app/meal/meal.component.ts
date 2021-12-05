371182bbbd743a72c4665bfcd9fbfb68ea9e1406
moctezumadaniel <moctezumadanielt@gmail.com>
2021-12-04 20:19:20 -0600

    logic to increase or decrease amounth of meals to order

--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -8,8 +8,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MealComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
-
+  maxAmounth: number = 10; //HACER DINAMICO
+  amounth: number = 1;
   ngOnInit(): void {
     console.log(this.route);
+  }
+  increment() {
+    if (this.amounth < this.maxAmounth) {
+      this.amounth++;
+    }
+    if (this.amounth === this.maxAmounth) {
+      return;
+    }
+    if (this.amounth > this.maxAmounth) {
+      this.amounth = this.maxAmounth;
+    }
+  }
+
+  decrease() {
+    if (this.amounth > 1) {
+      this.amounth--;
+    }
+    if (this.amounth === 1) {
+      return;
+    }
+    if (this.amounth < 1) {
+      this.amounth = 1;
+    }
   }
 }
